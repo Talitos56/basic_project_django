0ab6dd4c1c1c40b26046442b626503816e6128e7
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-07 17:43:02 -0300

    added new tasks

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    print('home')
+    return HttpResponse('home1')
+
+
+def blog(request):
+    print('blog')
+    return HttpResponse('blog')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', blog),
+    path('', home),
 ]
