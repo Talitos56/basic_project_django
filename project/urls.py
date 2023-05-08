20a421eb4c91d1454fbcfe296d911b28d87d8474
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-07 22:47:21 -0300

    added new files

--- project/urls.py
+++ project/urls.py
@@ -15,22 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
 
-
-def home(request):
-    print('home')
-    return HttpResponse('home1')
-
-
-def blog(request):
-    print('blog')
-    return HttpResponse('blog')
-
+from blog import views as blog_views
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog),
-    path('', home),
+    path('blog/', blog_views.blog),
+    path('', home_views.home),
 ]
