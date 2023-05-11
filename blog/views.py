bd06f839478aa21440b3d035e656897049a7d0cd
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-11 13:37:13 -0300

    added new tasks

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def blog(request):
     print('blog')
+
+    context = {
+        'text': 'Olá blog'
+    }
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 
 def exemplo(request):
     print('exemplo')
+
+    context = {
+        'text': 'Olá exemplo',
+        'title': 'Essa é uma página de exemplo - ',
+    }
+
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
