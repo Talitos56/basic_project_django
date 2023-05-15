e24aea8f5b92eef1984f444a12b8486595e76348
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-14 22:12:49 -0300

    added new tasks

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ app_name = 'blog'
 # Django URLs:
 # https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
-    path('<int:id>/', views.post, name='post'),
     path('', views.blog, name='home'),
+    path('<int:post_id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
